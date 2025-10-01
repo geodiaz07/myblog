b63b2947344e64657f59abd473a538e62c71b61c
Diaz Sumantri <diazgeografi07@gmail.com>
2025-10-01 09:51:13 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>DesaKita - Login</title>
+    <title>{{ config('app.name', 'Laravel') }} - Login</title>
     <meta name="description" content="MyBlog - Login Page" />
 
     <link rel="icon" href="https://placehold.co/16x16/0000FF/FFFFFF?text=My" type="image/x-icon" />
@@ -54,17 +54,17 @@
                 <div class="col-sm-10 col-md-8 col-lg-6 col-xl-4">
                     <div class="card shadow px-4 py-4">
                         <div class="card-body">
-                            <h2 class="card-title text-center mb-4 d-flex justify-content-center align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" class="text-primary me-3"
+                            <h2 class="card-title text-center mb-4 d-flex justify-content-center align-items-center text-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
-                                    class="icon icon-tabler icons-tabler-outline icon-tabler-files me-2">
+                                    class="icon icon-tabler icons-tabler-outline icon-tabler-key">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M15 3v4a1 1 0 0 0 1 1h4" />
-                                    <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
-                                    <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
+                                    <path
+                                        d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0z" />
+                                    <path d="M15 9h.01" />
                                 </svg>
-                                <span>MyBlog</span>
+                                <span class="ms-3">{{ config('app.name', 'Laravel') }}</span>
                             </h2>
                             <hr>
                             <p class="text-center text-muted mb-4">Login ke Akun Anda</p>
